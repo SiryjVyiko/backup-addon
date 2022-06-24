45af636ab39d5ddee86a4e01bc53413e36a8080f
SiryjVyiko <dz@hivext.net>
2022-06-24 15:49:10 +0300

    Update backup-manager.js

--- scripts/backup-manager.js
+++ scripts/backup-manager.js
@@ -82,7 +82,7 @@ function BackupManager(config) {
     me.checkCurrentlyRunningBackup = function () {
 	me.exec([
             [ me.cmd, [
-		        'PROCESS_STRING=$(ps aux|grep %(envName)_backup-logic.sh); if [ -n "${PROCESS_STRING}" ]; then echo "In_progress"; else true;'
+		'PROCESS_STRING=$(ps aux|grep %(envName)_backup-logic.sh); if [ -n "${PROCESS_STRING}" ]; then echo "In_progress"; else true'
             ], {
                 nodeId : config.backupExecNode,
                 envName : config.envName
